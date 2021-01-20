70ae08c4ca9cda3436847e0c72a719d254e56021
zhongzihan <1137882300@qq.com>
2021-01-20 10:08:24 +0800

    '首页开发'

--- README.md
+++ README.md
@@ -17,3 +17,8 @@ npm run build
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+
+//GitHub上创建好仓库后，与本地关联起来的命令
+git remote add origin https://github.com/1137882300/supermall.git
+git push -u origin master
---
+++ src/network/request.js
@@ -0,0 +1,32 @@
+import axios from 'axios'
+
+//最终的方式
+export function request(config) {
+    //1.创建axios的实例
+    const instance = axios.create({
+      baseURL: 'http://152.136.185.210:7878/api/m5',
+      timeout: 5000
+    })
+    //2.axios的请求拦截器
+    instance.interceptors.request.use(config =>{
+      console.log(config);
+      //1.比如config中的一些信息不符合服务器的要求
+      //2.比如每次发送网络请求时，都希望在界面显示一个请求的图标
+      //3.某些网络请求（登录（token））必须携带一些特殊的信息
+      return config;//释放 一定要
+    },err =>{
+      // console.log(err);
+    })
+
+    //响应拦截
+    instance.interceptors.response.use(res =>{
+      console.log(res);
+      return res.data
+    },err =>{
+      console.log(err);
+    })
+
+    //3.发送真正的网络请求
+    return  instance(config) // 本身返回的就是promise
+}
+
