29b85b1ff4b41da6212a2b81e743343c2e204aca
zhongzihan <1137882300@qq.com>
2021-01-25 12:24:09 +0800

    '首页基本完成'

---
+++ src/components/common/scroll/Scroll.vue
@@ -0,0 +1,36 @@
+<template>
+  <!-- ref /children 获取子组件-->
+  <div class="wrapper" ref="wrapper">
+    <div class="content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+  import BScroll from 'better-scroll'
+
+  export default {
+    name: "Scroll",
+    data(){
+      return {
+        scroll: null
+      }
+    },
+    mounted() {
+      this.scroll = new BScroll(this.$refs.wrapper,{
+        click: true
+      })
+
+    },
+    methods:{
+      scrollTo(x,y,time=1000){
+        this.scroll.scrollTo(x,y,time)
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
